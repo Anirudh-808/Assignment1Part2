429538f452f226c7d5f540a10c85969db956e2b3
Anirudh-808 <aninagaraj808@gmail.com>
2024-10-20 12:11:55 +0530

    1

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "github.com/gin-gonic/gin"
+
+type ID struct {
+	Username string
+	Password string
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/ping", IDHandler)
+	r.Run()
+}
+
+func IDHandler(c *gin.Context) {
+	var request ID
+	err := c.BindJSON(&request)
+
+	if err != nil {
+		c.JSON(400, gin.H{"error": "Invalid request format"})
+		return
+	}
+
+}
